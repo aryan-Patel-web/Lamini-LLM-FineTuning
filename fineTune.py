2f61267d5775c748d3bfb2ff054f86e1e1c5d2d7
aryan-Patel-web <aryanpatel77462@gmail.com>
2025-07-11 17:47:36 +0530

    fine-tune Lamini’s hosted Meta-Llama-3-8B-Instruct model on custom question-answer data

--- fineTune.py
+++ fineTune.py
@@ -44,7 +44,9 @@ def get_data():
     return data
 
 import lamini
+
 from lamini import Lamini
+
 lamini.api_key="6aeffd30d11098c222e641019dc4143882438e2801990e3e91e2d2d3ade153a9"
 
 llm=Lamini(model_name="meta-llama/Meta-Llama-3-8B-Instruct")
